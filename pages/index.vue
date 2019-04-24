b2a625043ab9e1bd40c9edd62ccc1571588c061f
Luis Chávez <luis.chavez24@unmsm.edu.pe>
2019-04-24 00:30:29 -0500

    Base del cliente Web

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <h1>Proyecto proface</h1>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+}
+</script>
