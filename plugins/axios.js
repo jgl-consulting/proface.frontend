f2d83dcdab2abc21db66ba92b6f4408c83b271d9
Luis Chávez <luchor13@gmail.com>
2019-08-13 21:04:52 -0500

    [Testing] Axios Plugin configure for custom loading component

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,9 +1,7 @@
 
 
-export default function ({ app, $axios, redirect, route }) {
+export default function ({ app, $axios, redirect }) {
   
-  let currentRequest = 0;
-
   // A noop loading inteterface for when $nuxt is not yet ready
   const noopLoading = {
     finish: () => { },
@@ -12,28 +10,54 @@ export default function ({ app, $axios, redirect, route }) {
     set: () => { }
   }
 
-  
   function isManualPageLoading(window) {
     return window.$nuxt.$root.$loading.manual !== undefined && window.$nuxt.$root.$loading.manual === false;
   }
+  
   const $loading = ($window) => isManualPageLoading($window) ? noopLoading : $window.$nuxt.$root.$loading;
+  
+  $axios.onRequest(config => {
+    
+    if (config && config.progress === false) {
+      return;
+    }
+    if (process.browser && window.$nuxt) {
+      $loading(window).start();
+    }
+
+  });
+
+  $axios.onResponse(response => {
+
+    if(response && response.config && response.config.progress === false){
+      return;
+    }
+
+    if (process.browser && window.$nuxt) {
+      $loading(window).finish();
+    }
+
+  });
 
   $axios.onError(error => {
-    if(currentRequest > 0) {
-      currentRequest--;
-    }
     
+    if (error && error.config && error.config.progress === false) {
+      return
+    } 
+
     if (process.browser && window.$nuxt) {
       $loading(window).fail();
     }
-
+    
     if(error.response) {
       if(error.response.status === 500) {
         redirect('/sorry');
       } else if(error.response.status === 401) {
         redirect('/unauthorized');
-      } else if(route.fullPath !== '/login'){
-        /* const { message, errors } = error.response.data;
+      } 
+      /* 
+      else if(route.fullPath !== '/login'){
+        const { message, errors } = error.response.data;
         if(message && errors) {
           const allErrors = errors.map(e => e.errorMessage).join("\n");
           app.$confirm(allErrors, {
@@ -43,24 +67,13 @@ export default function ({ app, $axios, redirect, route }) {
           });
         } else {
           app.$confirm(error, { title: "Ocurrió un error", color: "error", width: 500 });
-        } */
-      }
+        }
+      } 
+      */
     }
   });
 
-  $axios.onRequest(() => {
-    currentRequest ++;
-    if (process.browser && window.$nuxt) {
-      $loading(window).start();
-    }
-  });
-
-  $axios.onResponse(() => {
-    if(currentRequest > 0) {
-      currentRequest--;
-    }
-    if (process.browser && window.$nuxt) {
-      $loading(window).finish();
-    }
-  })
+  function logRequest() {
+    console.log(`Request [${currentRequest}]`);
+  }
 }
